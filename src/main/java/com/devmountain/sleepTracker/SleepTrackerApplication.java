c2eb73e96b54844c5e42fa8b510f0d31d5132432
Ikram Ahmed <ikramsadiq200@gmail.com>
2022-10-25 22:56:34 -0500

    initial commit

---
+++ src/main/java/com/devmountain/sleepTracker/SleepTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.devmountain.sleepTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SleepTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SleepTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devmountain/sleepTracker/SleepTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devmountain.sleepTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SleepTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
