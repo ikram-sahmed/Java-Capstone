549fb037d5f7ed39dc8da99eec3048953c66f12b
Ikram Ahmed <ikramsadiq200@gmail.com>
2022-11-03 23:24:56 -0500

    home page html/css done

--- src/main/resources/static/javascript/home.js
+++ src/main/resources/static/javascript/home.js
@@ -1,5 +1,5 @@
-const registerButton = document.getElementById('register');
-const loginButton = document.getElementById('login');
+const registerButton = document.getElementById('Register');
+const loginButton = document.getElementById('Login');
 const container = document.getElementById('container');
 
 registerButton.addEventListener('click', () => {
@@ -9,4 +9,68 @@ registerButton.addEventListener('click', () => {
 loginButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
 });
+
+
+const registerUsername = document.getElementById('register-username')
+const registerPassword = document.getElementById('register-password')
+const registerEmail = document.getElementById ('email')
+const loginUsername = document.getElementById("username");
+const loginPassword = document.getElementById("password");
+
+
+const headers = {
+    'Content-Type':'application/json'
+}
+
+const baseUrl = 'http://localhost:8080/api/v1/users'
+
+
+const handleButtonRegister = async (e) =>{
+    e.preventDefault()
+
+    let bodyObj = {
+        username: registerUsername.value,
+        password: registerPassword.value,
+        email: registerEmail.value
+    }
+
+    const response = await fetch(`${baseUrl}/register`, {
+        method: "POST",
+        body: JSON.stringify(bodyObj),
+        headers: headers
+    })
+        .catch(err => console.error(err.message))
+
+    const responseArr = await response.json()
+
+    if (response.status === 200){
+        window.location.replace(responseArr[0])
+    }
+}
+
+const handleButtonLogin = async (e) => {
+    e.preventDefault()
+    let bodyObj = {
+        username: loginUsername.value,
+        password: loginPassword.value
+    }
+
+    const response = await fetch(`${baseUrl}/login`, {
+        method: "POST",
+        body: JSON.stringify(bodyObj),
+        headers: headers
+    })
+        .catch(err => console.error(err.message))
+
+    const responseArr = await response.json()
+
+    if (response.status === 200){
+        document.cookie = `userId=${responseArr[1]}`
+        window.location.replace(responseArr[0])
+    }
+
+}
+
+registerButton.addEventListener("Register", handleButtonRegister)
+loginButton.addEventListener("Login", handleButtonLogin);
 
